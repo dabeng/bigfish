6d7d8f4f7fe9b58e5bc7408f7b47cf6005dd751d
dabeng <dabeng413@gmail.com>
2018-06-15 15:26:42 +0800

    WIP: add topic page

---
+++ src/components/CreateTopic.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <input type="text" class="editor" v-model="topic">
+    <textarea class="editor" v-model="cotent"></textarea>
+  </div>
+</template>
+<style>
+</style>
+<script>
+import {db} from '../firebase'
+
+export default {
+  name: 'BaseTable',
+  data: function () {
+    return {
+      topic: '',
+      cotent: ''
+    }
+  }
+}
+</script>
--- src/components/FishPage.vue
+++ src/components/FishPage.vue
@@ -2,14 +2,22 @@
 <div class="fish-define">
   <h1 class="subject">鱼</h1>
   <p>{{id}}</p>
+  <TopicList/>
+  <CreateTopic/>
 </div>
 </template>
 <style scoped>
 </style>
 <script>
+import TopicList from './TopicList.vue'
+import CreateTopic from './CreateTopic.vue'
 
 export default {
   name: 'FishPage',
-  props: ['id']
+  props: ['id'],
+  components: {
+    TopicList,
+    CreateTopic
+  }
 }
 </script>
